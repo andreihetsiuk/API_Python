f2eeb377143e6ec821c5be88b6214a58a2ef1adb
AndreiHetsiuk <andrei.hetsiuk@gmail.com>
2022-01-03 15:08:13 +0300

    if check is added

---
+++ json_parsing.py
@@ -0,0 +1,13 @@
+import json
+
+string_as_json_format = '{"answer": "Hello, user"}'
+obj = json.loads(string_as_json_format)
+
+key = "answer"
+
+if key in obj:
+    print(obj[key])
+else:
+    print(f"key {key} is absent in JSON")
+
+
