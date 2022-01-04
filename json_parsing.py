29d41586360dcbd77a314df41ae83fdd57a71377
AndreiHetsiuk <andrei.hetsiuk@gmail.com>
2022-01-04 14:29:22 +0300

    ex5 answer

--- json_parsing.py
+++ json_parsing.py
@@ -1,12 +1,12 @@
 import json
 
-string_as_json_format = '{"answer": "Hello, user"}'
+string_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(string_as_json_format)
 
-key = "answer"
+key = "messages"
 
 if key in obj:
-    print(obj[key])
+    print(obj[key][1])
 else:
     print(f"key {key} is absent in JSON")
 
