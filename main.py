58a9894c8ce81f32549040039b553c70f73d3be5
AndreiHetsiuk <andrei.hetsiuk@gmail.com>
2022-01-05 19:11:23 +0300

    ex7 answer

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import requests
 
-# payload = {"name": "user"}
+#1
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+print(response.text)
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-print(response.history)
-print(response.history[2].url)
+#2
+response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={'method': 'HEAD'})
+print(response.text)
+
+#3
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={'method': 'GET'})
+print(response.text)
+
+
+# 4
+print('-------GET--------')
+for i in ('POST', 'DELETE', 'PUT', 'GET'):
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={'method': i})
+    print(response.text)
+print('------DELETE---------')
+for i in ('POST', 'DELETE', 'PUT', 'GET'):
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={'method': i})
+    print(response.text)
+print('------PUT---------')
+for i in ('POST', 'DELETE', 'PUT', 'GET'):
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={'method': i})
+    print(response.text)
+print('-------POST--------')
+for i in ('POST', 'DELETE', 'PUT', 'GET'):
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={'method': i})
+    print(response.text)
