e55c5165dba9012c24763747ea9e536fd6125341
AndreiHetsiuk <andrei.hetsiuk@gmail.com>
2022-01-05 13:47:59 +0300

    ex6 answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
-payload = {"name": "user"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(response.text)
+# payload = {"name": "user"}
+
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+print(response.history)
+print(response.history[2].url)
