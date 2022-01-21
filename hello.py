dec5d32ca24332725050c66952d50ef08e901b1a
AndreiHetsiuk <andrei.hetsiuk@gmail.com>
2022-01-21 17:52:18 +0300

    deleted files

--- hello.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def test_auth_user(self):
-    data = {
-        'email': 'vinkotov@example.com',
-        'password': '1234'
-    }
-    response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
-    print(response1.text)
