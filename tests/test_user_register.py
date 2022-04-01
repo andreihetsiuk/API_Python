ed9be011fdfa835bdb5e7ff09bbb14af32f73bc0
AH <andrei.hetsiuk@gmail.com>
2022-04-01 20:00:04 +0300

    added test for creating a new user

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,18 +1,42 @@
 import requests
 from lib.BaseCase import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
+    def setup(self):
+        base_type = "learn"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_type}{random_part}@{domain}"
+
+
+    def test_create_new_user(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.assert_status_code(response, 200)
+
+        Assertions.assert_json_has_key(response, "id")
+
+        print(response.content)
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
-            'password': 'user',
+            'password': '123',
             'username': 'learnqa',
             'firstName': 'learnqa',
             'lastName': 'learnqa',
             'email': email
         }
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
-        assert response.status_code == 400, f"status code isn't 400"
+
+        Assertions.assert_status_code(response,400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected content respond {response.content}"
