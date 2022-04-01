fa4fb832f9bec378b1c3b8eb95cd0af7aa040f19
AH <andrei.hetsiuk@gmail.com>
2022-04-01 18:20:01 +0300

    adding an existing user test. lesson 4 vid 1

---
+++ tests/test_user_register.py
@@ -0,0 +1,18 @@
+import requests
+from lib.BaseCase import BaseCase
+from lib.assertions import Assertions
+
+
+class TestUserRegister(BaseCase):
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data = {
+            'password': 'user',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        assert response.status_code == 400, f"status code is not 400"
+        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected content respond {response.content}"
