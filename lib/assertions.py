ede59fd7154372b5bfea7e8ae8f865242f51587d
AH <andrei.hetsiuk@gmail.com>
2022-04-01 20:01:40 +0300

    added assertions for status code and assert that json has a key

--- lib/assertions.py
+++ lib/assertions.py
@@ -14,3 +14,22 @@ class Assertions:
         assert name in response_as_dict, f'Response does not have {name}'
         assert response_as_dict[name] == expected_value, error_message
 
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+
+            assert False, f'response is not in json format, response is {response.text}'
+
+        assert name in response_as_dict, f'Response does not have {name}'
+
+    @staticmethod
+    def assert_status_code(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code. Expected: {expected_status_code},Actual: {response.status_code}"
+
+
+
+
+
