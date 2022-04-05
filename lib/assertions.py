a8894b9ec6dca92d8aed63c804d6cffda7da2017
AH <andrei.hetsiuk@gmail.com>
2022-04-05 18:39:50 +0300

    added: 1. test_user_get_details_not_auth 2. test_user_get_details_auth_as_same_user 3. assert_json_has_keys to Assertions

--- lib/assertions.py
+++ lib/assertions.py
@@ -25,11 +25,26 @@ class Assertions:
         assert name in response_as_dict, f'Response does not have {name}'
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+
+            assert False, f'response is not in json format, response is {response.text}'
+        for name in names:
+            assert name in response_as_dict, f'Response does not have {name}'
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+
+            assert False, f'response is not in json format, response is {response.text}'
+
+        assert name not in response_as_dict, f'Response shouldnt  have {name} but its present'
+
+    @staticmethod
     def assert_status_code(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code. Expected: {expected_status_code},Actual: {response.status_code}"
-
-
-
-
-
